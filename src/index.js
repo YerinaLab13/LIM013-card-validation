7dbaae240bc60306cc925753802b48d69e203105
YerinaLab13 <yerinaig4@gmail.com>
2020-07-23 22:45:39 -0500

    primera pag html-js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
+//console.log(validator);
+    // const boton = document.getElementById("boton");
+    // boton.addEventListener("click",() => {
+    // let datos = document.getElementById("datos");
+    // let numbercard = document.getElementById("number").Value;
+    // console.log (numbercard);
+    // });
+    
+    // document.getElementById("inicio").style.display = "none";
+    // document.getElementById("datos").style.display = "block";
 
-console.log(validator);
+    
+    const show = ()=>{
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("datos").style.display = "block";
+    }
+    document.getElementById("boton").addEventListener("click",show);
+
+    document.getElementById("send").addEventListener("click", (event)=>{
+        event.preventDefault()
+        let numberCard = document.getElementById("numberCard").value;
+        // console.log(numberCard);
+        validator.isValid(numberCard);
+        validator.maskify(numberCard);
+    });
+    
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,21 @@
 const validator = {
   // ...
+  isValid: function(num){
+    console.log(num)
+  },
+  maskify:function(num){
+    let maskify2 = '';
+    for(let i=0 ; i < num.length ; i++){
+      if(i <= num.length -5 ){
+          maskify2 = maskify2 + '#';
+      }else{
+          maskify2 = maskify2 + num[i];
+      }
+        console.log(maskify2)
+    }
+      return maskify2
+  }
+
 };
 
 export default validator;
